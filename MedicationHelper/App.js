18141cbee2a42db668224dea90ee8709cb3aac4c
BaekInHyeok <bih63879@gmail.com>
2023-04-11 01:52:42 +0900

    Nativ-Base 연동 및 테스트

--- MedicationHelper/App.js
+++ MedicationHelper/App.js
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+import { NativeBaseProvider, Box, Input } from "native-base";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Text>Medication Helper</Text>
+      <Box alignItems="center">
+        <Input mx="3" placeholder="ID" w="100%" />
+        <Input mx="3" placeholder="PW" w="100%" />
+      </Box>
+    </NativeBaseProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
